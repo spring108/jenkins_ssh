18e3d40444484e90632f5b525ccea1a855cd7bc5
Igor Skripnikov <garik108@narod.ru>
2023-12-27 20:36:37 +0300

    prod script

--- prod.sh
+++ prod.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
 cd /etc/jenkins_ssh
 
+echo '{"insecure-registries" : ["158.160.108.198:8123"]}' > /etc/docker/daemon.json
+service docker restart
 
+#docker login 158.160.108.198:8123 --username admin --password 34-Nexus-!
+#echo 'docker login success'
 
-
-
+docker run -d -p 80:80 158.160.108.198:8123/myweb:$my_version
