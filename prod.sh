60f38a6a20e842ba70de7d1bac1123b7591c4300
Igor Skripnikov <garik108@narod.ru>
2023-12-27 20:41:28 +0300

    add parameter

--- prod.sh
+++ prod.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+my_version=$1
+
+if [ "$my_version" = "" ]
+then
+	echo 'No version in first parameter'
+	exit
+fi
+
+
 cd /etc/jenkins_ssh
 
 echo '{"insecure-registries" : ["158.160.108.198:8123"]}' > /etc/docker/daemon.json
@@ -8,5 +17,5 @@ service docker restart
 docker login 158.160.108.198:8123 --username admin --password 34-Nexus-!
 echo 'docker login success'
 
-docker pull 158.160.108.198:8123/myweb:latest
-docker run -d -p 80:80 158.160.108.198:8123/myweb:latest
+docker pull 158.160.108.198:8123/myweb:$my_version
+docker run -d -p 80:80 158.160.108.198:8123/myweb:$my_version
