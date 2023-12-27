31a223f52af2523ce2e3d94d05fe261a9b1c9478
Igor Skripnikov <garik108@narod.ru>
2023-12-27 20:38:07 +0300

    docker run :latest

--- prod.sh
+++ prod.sh
@@ -8,4 +8,4 @@ service docker restart
 #docker login 158.160.108.198:8123 --username admin --password 34-Nexus-!
 #echo 'docker login success'
 
-docker run -d -p 80:80 158.160.108.198:8123/myweb:$my_version
+docker run -d -p 80:80 158.160.108.198:8123/myweb:latest
