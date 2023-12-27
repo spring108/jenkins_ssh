dcbb096777a7fed08b1f018c5a7c0587af033450
Igor Skripnikov <garik108@narod.ru>
2023-12-27 20:39:53 +0300

    add docker auth

--- prod.sh
+++ prod.sh
@@ -5,8 +5,8 @@ cd /etc/jenkins_ssh
 echo '{"insecure-registries" : ["158.160.108.198:8123"]}' > /etc/docker/daemon.json
 service docker restart
 
-#docker login 158.160.108.198:8123 --username admin --password 34-Nexus-!
-#echo 'docker login success'
+docker login 158.160.108.198:8123 --username admin --password 34-Nexus-!
+echo 'docker login success'
 
 docker pull 158.160.108.198:8123/myweb:latest
 docker run -d -p 80:80 158.160.108.198:8123/myweb:latest
